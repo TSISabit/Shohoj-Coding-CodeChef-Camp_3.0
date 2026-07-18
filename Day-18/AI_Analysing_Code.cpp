33c9f9ed6aaafca8593d11f8205b0b5a0b91d980
TSISabit <tariksaidulislam@gmail.com>
2026-07-18 17:55:25 +0600

    Added Day-18

---
+++ Day-18/AI_Analysing_Code.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define nn "\n" 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define read(x) int x; cin >> x;
+#define readv(v, n) vector<int> v(n); for (auto &i : v) cin >> i;
+#define sz(x) (int)(x).size()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define vi vector<int>
+
+void solve() {
+    int c; cin >> c; 
+    if(c <= 1000) yes else no 
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // int t; cin >> t;
+    // while (t--) {
+        solve();
+    // }
+    return 0;
+}
---
+++ Day-18/TV_Discount.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define nn "\n" 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define read(x) int x; cin >> x;
+#define readv(v, n) vector<int> v(n); for (auto &i : v) cin >> i;
+#define sz(x) (int)(x).size()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define vi vector<int>
+
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if(a - c < b - d) cout << "First" << endl;
+    else if(a - c == b - d) cout << "Any" << endl;
+    else cout << "Second" << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
