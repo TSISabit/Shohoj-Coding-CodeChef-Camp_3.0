2dcac736ecfc8beef86025e85e9165f7ae6970d7
TSISabit <tariksaidulislam@gmail.com>
2026-07-25 14:26:05 +0600

    Added Day-21

---
+++ Day-21/Rivalry.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define nn "\n" 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define read(x) int x; cin >> x;
+#define readv(v, n) vector<int> v(n); for (auto &i : v) cin >> i;
+#define sz(x) (int)(x).size()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define vi vector<int>
+
+void solve() {
+    int r1, r2, d1, d2; 
+    cin >> r1 >> r2 >> d1 >> d2; 
+    int Dominater = r1 + d1; 
+    int Everule = r2 + d2; 
+    if(Dominater > Everule) cout << "Dominater\n"; 
+    else if(Dominater < Everule) cout << "Everule\n"; 
+    // else cout << ""
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // int t; cin >> t;
+    // while (t--) {
+        solve();
+    // }
+    return 0;
+}
