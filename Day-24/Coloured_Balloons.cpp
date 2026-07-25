f3babeb6b974e55475514d14fb871b5a62979538
TSISabit <tariksaidulislam@gmail.com>
2026-07-25 20:16:08 +0600

    Added Day-24

---
+++ Day-24/Coloured_Balloons.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define nn "\n" 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define read(x) int x; cin >> x;
+#define readv(v, n) vector<int> v(n); for (auto &i : v) cin >> i;
+#define sz(x) (int)(x).size()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define vi vector<int>
+
+void solve() {
+    read(n); 
+    vector<int> a(n + 1); 
+    int price = 0; 
+    for(int i = 1; i <= n; i++){
+        cin >> a[i]; 
+        price += (i * a[i]); 
+    }
+    cout << price << nn; 
+
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
